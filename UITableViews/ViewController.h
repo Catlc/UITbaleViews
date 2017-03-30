72359cf0122cf65128476de292d4b433a55856c4
ya-eternity <yeziyan825@gmail.com>
2017-03-30 09:20:21 +0800

    注释语句

---
+++ UITableViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViews
+//
+//  Created by bmxd-002 on 17/3/30.
+//  Copyright © 2017年 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViews
+//
+//  Created by bmxd-002 on 17/3/30.
+//  Copyright © 2017年 LC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
