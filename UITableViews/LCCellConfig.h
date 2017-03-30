6bb9a2076bab6eb48ab7026326ece190ec524b0d
ya-eternity <yeziyan825@gmail.com>
2017-03-30 10:57:29 +0800

    敲完LCCellConfig方法

--- UITableViews/LCCellConfig.h
+++ UITableViews/LCCellConfig.h
@@ -6,8 +6,68 @@
 //  Copyright © 2017年 LC. All rights reserved.
 //
 
+/**
+     Config - 配置
+ */
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 @interface LCCellConfig : NSObject
+
+/// cell 类名
+@property (nonatomic,strong)NSString *className;
+
+///标题 -
+@property (nonatomic,strong)NSString *title;
+
+///显示模型的方法
+@property(nonatomic ,assign)SEL showInfoMethod;
+
+/// Cell高度（固定高度）
+@property(nonatomic,assign)CGFloat heightOfCell;
+
+/// 预留属性detail
+@property(nonatomic,strong)NSString *detail;
+
+///指定重用ID 不赋值使用类名
+@property(nonatomic,strong)NSString *reuseID;
+
+#pragma mark -Core
+
+/**
+ @brief 便利构造器
+
+ @param className 类名
+ @param title 标题
+ @param showInfoMethod 显示数据模型
+ @param heightOfCell cell高度
+ @return instancetype
+ */
+
++ (instancetype)cellComfigWithClassName:(NSString *)className
+                                 title:(NSString *)title
+                        showInfoMethod:(SEL)showInfoMethod
+                          heightOfCell:(CGFloat)heightOfCell;
+/// 根据cellConfig 生成Cell，重用ID为cell类名
+- (UITableViewCell *)cellOfCellConfigWithTableView:(UITableView *)tableView
+                                         dataModel:(id)dataModel;
+
+/// 根据cellConfig 生成Cell，重用ID为cell类名 可用Nib
+- (UITableViewCell *)cellOfCellConfigWithTableView:(UITableView *)tableView
+                                         dataModel:(id)dataModel
+                                             isNib:(BOOL)isNib;
+
+#pragma mark - Dynamic Height 
+/// cell动态高度（计算后缓存到这个字段，避免重复计算）
+@property (nonatomic,assign)CGFloat dynamicHeightOfCell;
+
+///缓存高度
+- (CGFloat)heightCachedWothCalculateBlock:(CGFloat (^)(void))block;
+
+#pragma mark -Assist
+/// 根据类名，快捷注册cell
+- (void)registerForTableView:(UITableView *)tableView;
+
+
 
 @end
--- UITableViews/LCCellConfig.m
+++ UITableViews/LCCellConfig.m
@@ -10,4 +10,100 @@
 
 @implementation LCCellConfig
 
+/**
+ @brief 便利构造器
+ 
+ @param className 类名
+ @param title 标题
+ @param showInfoMethod 显示数据模型
+ @param heightOfCell cell高度
+ @return instancetype
+ */
+
++(instancetype)cellComfigWithClassName:(NSString *)className title:(NSString *)title showInfoMethod:(SEL)showInfoMethod heightOfCell:(CGFloat)heightOfCell
+{
+    LCCellConfig *cellConfig = [LCCellConfig new];
+    
+    cellConfig.className = className;
+    cellConfig.title = title;
+    cellConfig.showInfoMethod = showInfoMethod;
+    cellConfig.heightOfCell = heightOfCell;
+    
+    return cellConfig;
+}
+/// 根据cellConfig生成cell，重用ID为cell类名
+-(UITableViewCell *)cellOfCellConfigWithTableView:(UITableView *)tableView dataModel:(id)dataModel
+{
+    return [self cellOfCellConfigWithTableView:tableView dataModel:dataModel isNib:NO];
+}
+/// 根据cellConfig生成cell，重用ID为cell类名,可使用Nib
+-(UITableViewCell *)cellOfCellConfigWithTableView:(UITableView *)tableView dataModel:(id)dataModel isNib:(BOOL)isNib
+
+{
+    
+    Class cellClass = NSClassFromString(self.className);
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self cellID]];
+    
+    if (!cell) {
+        
+        //加入使用nib的方法
+        if (isNib && self.className.length&&![self.className isEqualToString:@"UITableViewCell"]) {
+            NSArray *nibs = [[NSBundle mainBundle]loadNibNamed:self.className owner:nil options:nil];
+            
+            for (id obj in nibs) {
+                if ([obj isKindOfClass:cellClass]) {
+                    cell = obj;
+                }
+            }
+
+            if (!cell) {
+                NSLog(@"Please Check Nib File About %@",cellClass);
+            }
+            
+        } else
+        {
+            cell = [[cellClass ?:[UITableView class] alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:[self cellID]];
+        }
+    }
+    
+    if (self.showInfoMethod &&[cell respondsToSelector:self.showInfoMethod]) {
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [cell performSelector:self.showInfoMethod withObject:dataModel];
+#pragma clang diagnostic pop
+
+    }
+    
+    return cell;
+    
+}
+#pragma mark - Dynamic Height 
+/// 缓存高度
+- (CGFloat)heightCachedWothCalculateBlock:(CGFloat (^)(void))block
+{
+    if (!self.dynamicHeightOfCell && block) {
+        //没有计算过高度
+        //计算高度并保存
+        self.dynamicHeightOfCell = block();
+        
+    }
+    return self.dynamicHeightOfCell;
+    
+}
+#pragma  mark - Assist
+/// 根据类名，快捷注册cell
+-(void)registerForTableView:(UITableView *)tableView
+{
+    [tableView registerNib:[UINib nibWithNibName:self.className bundle:nil] forCellReuseIdentifier:[self cellID]];
+}
+-(NSString *)cellID
+{
+    if (self.reuseID.length) {
+        return  self.reuseID;
+    }
+    return self.className;
+    
+}
 @end
